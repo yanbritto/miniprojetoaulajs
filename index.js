3135eef37eab8cdd9031093d78b2c231fa98e944
Yan <yanbritto44@gmail.com>
2025-04-29 14:28:10 -0300

    projeto aula

---
+++ index.js
@@ -0,0 +1,10 @@
+function novaTarefa(){
+    overlay.classList.add('active');
+    criarTarefa.classList.add('active');
+}
+
+
+function fecharModal(){
+    overlay.classList.remove('active');
+    criarTarefa.classList.remove('active');
+}
