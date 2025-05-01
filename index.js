3dc81528900ed20576bb133f720d94e265bc1862
Yan <yanbritto44@gmail.com>
2025-05-01 00:16:12 -0300

    fim do mini pojeto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function novaTarefa(){
+function abriModal(){
     overlay.classList.add('active');
     criarTarefa.classList.add('active');
 }
@@ -27,12 +27,66 @@ function inserirTarefas(listaDeTarefas){
                     <h5>${tarefa.titulo}</h5>
                     <p>${tarefa.descricao}</p>
                     <div class="actions">
-                     <box-icon name='trash-alt' size="sm"></box-icon>
+                     <box-icon name='trash-alt' size="sm" onclick="deletarTarefa(${tarefa.id})"></box-icon>
                     </div>
              </li>
             `;
         })
 
+    }
+    
+}
+
+function novaTarefa(){
+    event.preventDefault();
+    let tarefa = {
+        titulo: titulo.value,
+        descricao: descricao.value
+
+    }
+    fetch("http://localhost:3000/tarefas",{
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(tarefa)
+    })
+    .then(res => res.json())
+    .then(res => {
+        fecharModal();
+        buscarTarefas();
+        let form = document.querySelector("#criarTarefa form");
+        form.reset();
+    })
+
+
+
+   
+
+}
+
+function deletarTarefa(id){
+     fetch(`http://localhost:3000/tarefas/${id}`,{
+        method: "DELETE",
+     })
+     .then(res => res.json)
+     .then(res => {
+        alert("tarefa deletada com sucesso");
+        buscarTarefas();
+     })
+}
+
+function pesquisarTarefas(){
+    let lis = document.querySelectorAll("ul li");
+    console.log(lis)
+    if(busca.value.length > 0){
+        lis.forEach(li =>{
+            if(!li.children[0].innerText.includes(busca.value)){
+                li.classList.add('oculto');
+            }else{
+                li.classList.remove('oculto');
+            }
+        })
     }
     
 }
