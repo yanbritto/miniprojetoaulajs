d932344824114e23168d2f38bb467b47e12e5381
Yan <yanbritto44@gmail.com>
2025-04-30 22:33:32 -0300

    adicionado funções add e buscar tarefas

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ function fecharModal(){
     overlay.classList.remove('active');
     criarTarefa.classList.remove('active');
 }
+
+function buscarTarefas(){
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res =>{
+        inserirTarefas(res);
+
+    })
+}buscarTarefas();
+
+function inserirTarefas(listaDeTarefas){
+    if(listaDeTarefas.length > 0){
+        lista.innerHTML=""
+        listaDeTarefas.map(tarefa =>{
+            lista.innerHTML += `
+              <li>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                    <div class="actions">
+                     <box-icon name='trash-alt' size="sm"></box-icon>
+                    </div>
+             </li>
+            `;
+        })
+
+    }
+    
+}
